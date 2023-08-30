a538bd7589f8ba054e88eb74506dcb02cf58edf0
Joshua de Wet <141920614+jdewera@users.noreply.github.com>
2023-08-30 14:53:18 +1200

    Add benchmarks

---
+++ test/Mercury.Benchmarks/MemoryScannerBenchmarks.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using BenchmarkDotNet.Attributes;
+
+namespace Mercury.Benchmarks;
+
+[MemoryDiagnoser]
+public class MemoryScannerBenchmarks
+{
+    [Params(1, 10, 100, 1000)]
+    public int PatternLength { get; set; }
+
+    private string _pattern = null!;
+    private GCHandle _patternHandle;
+
+    [Benchmark]
+    public void FindPattern()
+    {
+        MemoryScanner.FindPattern(Process.GetCurrentProcess(), _pattern);
+    }
+
+    [GlobalCleanup]
+    public void GlobalCleanup()
+    {
+        _patternHandle.Free();
+    }
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        var patternBytes = RandomNumberGenerator.GetBytes(PatternLength);
+
+        _pattern = BitConverter.ToString(patternBytes).Replace('-', ' ');
+        _patternHandle = GCHandle.Alloc(patternBytes, GCHandleType.Pinned);
+    }
+}
---
+++ test/Mercury.Benchmarks/Program.cs
@@ -0,0 +1,4 @@
+﻿using BenchmarkDotNet.Running;
+using Mercury.Benchmarks;
+
+BenchmarkRunner.Run<MemoryScannerBenchmarks>();
