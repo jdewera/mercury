026306141c3579bbc58ab88b6f033a1113bcc90f
Joshua de Wet <141920614+jdewera@users.noreply.github.com>
2023-09-22 08:52:34 +1200

    Swap to positional record syntax

--- src/Mercury/Native/Structs/MemoryBasicInformation.cs
+++ src/Mercury/Native/Structs/MemoryBasicInformation.cs
@@ -4,17 +4,4 @@ using Mercury.Native.Enums;
 namespace Mercury.Native.Structs;
 
 [StructLayout(LayoutKind.Explicit, Size = 48)]
-internal readonly record struct MemoryBasicInformation64
-{
-    [field: FieldOffset(0x0)]
-    internal long BaseAddress { get; init; }
-
-    [field: FieldOffset(0x18)]
-    internal nint RegionSize { get; init; }
-
-    [field: FieldOffset(0x20)]
-    internal PageState State { get; init; }
-
-    [field: FieldOffset(0x24)]
-    internal PageProtection Protect { get; init; }
-}
+internal readonly record struct MemoryBasicInformation64([field: FieldOffset(0x0)] long BaseAddress, [field: FieldOffset(0x18)] nint RegionSize, [field: FieldOffset(0x20)] PageState State, [field: FieldOffset(0x24)] PageProtection Protect);
